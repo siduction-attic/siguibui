12ff524134bec619ca35f3a696ccdfcf1ccb5a4d
Ovidiu Angelescu <convbsd@siduction.org>
2014-08-31 20:47:03 +0300

    changed automount-control.sh for gnome, mate and cinnamon

--- backend/automount-control.sh
+++ backend/automount-control.sh
@@ -1,34 +1,46 @@
 #!/bin/sh
-# Switches automounting on or off (for gnome)
-set -x
+# Switches automounting on or off (for gnome, cinnamon and mate)
+#set -x
 date "+%Y.%m.%d %H:%M:%S: $1" >>/tmp/automount.log
 
-if [ ! -x /usr/bin/gsettings ] ; then
-	exit 2
-else
-	if [ -f /etc/default/distro ]; then
-		. /etc/default/distro
-	fi
-
-	#if [ ! -f /etc/default/fll-gnome-desktop ]; then
-		session=$(pgrep -u "${FLL_LIVE_USER}" session)
-		export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$session/environ)
-		#test -z $VERBOSE || echo "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
-	#fi
-
-	case "$1" in
-		disabled)
-			su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount-open false"
-		;;
-		enabled)
-			su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount-open true"
-		;;
-		*)
-			echo "automount-control.sh called with unknown argument \`$1'" >&2
-			exit 1
-		;;
-	esac
+if [ -f /etc/default/distro ]; then
+    . /etc/default/distro
 fi
-set +x
 
+get_dbus_session()
+{
+    session=$(pgrep -u "${FLL_LIVE_USER}" session)
+    export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$session/environ)
+}
 
+case "$1" in
+    enabled)
+        SET_AUTOMOUNT=true
+        ;;
+    disabled)
+        SET_AUTOMOUNT=false
+        ;;
+    *)
+        echo "automount-control.sh called with unknown argument \`$1'" >&2
+        exit 1
+        ;;
+esac
+
+case "$FLL_FLAVOUR" in
+    gnome)
+        get_dbus_session
+        su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount $SET_AUTOMOUNT"
+        su ${FLL_LIVE_USER} -c "gsettings set org.gnome.desktop.media-handling automount-open $SET_AUTOMOUNT"
+        ;;
+    cinnamon)
+        get_dbus_session
+        su ${FLL_LIVE_USER} -c "gsettings set org.cinnamon.desktop automount $SET_AUTOMOUNT"
+        su ${FLL_LIVE_USER} -c "gsettings set org.cinnamon.desktop automount-open $SET_AUTOMOUNT"
+        ;;
+    mate)
+        get_dbus_session
+        su ${FLL_LIVE_USER} -c "gsettings set org.mate.media-handling automount $SET_AUTOMOUNT"
+        su ${FLL_LIVE_USER} -c "gsettings set org.mate.media-handling automount-open $SET_AUTOMOUNT"
+        ;;
+esac
+#set +x
